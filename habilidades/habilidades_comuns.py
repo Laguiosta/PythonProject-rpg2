b0e7dd5b6948e365f9639cd1fb53dea2386f7518
AdrianoLaguiosta <adriano.laguiosta@gmail.com>
2026-03-16 15:30:59 -0300

    Remove combat, inventory, item, character, and ability classes from the engine and interface modules, streamlining the codebase by eliminating unused components.

--- habilidades/habilidades_comuns.py
+++
@@ -1,6 +0,0 @@
-from habilidades.habilidade_base import Habilidade
-
-
-ataque_rapido = Habilidade('Ataque rápido', 'Um golpe muito rápido', 1, 5, 3)
-soco = Habilidade('Soco', 'Um soco forte', 1, 1, 3)
-    
--- utils/cores.py
+++
@@ -1,6 +0,0 @@
-class Cores:
-    VERMELHO = "\033[91m"
-    VERDE = "\033[92m"
-    AMARELO = "\033[93m"
-    AZUL = "\033[94m"
-    RESET = "\033[0m"
